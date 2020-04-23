06e59afefa3d42b6ca1a06d35bc888fefeb4a8af
brajesh <bk04031997@gmail.com>
2020-04-23 20:02:32 +0530

    Backend_setup

---
+++ backend_api/APIMARKETPLACE/src/main/java/com/java/server/ApimarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.java.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimarketplaceApplication.class, args);
+	}
+
+}
